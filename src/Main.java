c2d45d17de61b8c1b804c9538b3db1ba7ab75694
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-28 16:06:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int spring = 274;
+        int summer = 192;
+        int fall = 129;
+        int winter = 356;
+        int total = spring + summer + fall + winter;
+
+
+        System.out.println("The home maintenance cost for the spring season is " + spring);
+        System.out.println("The home maintenance cost for the summer season is " + summer);
+        System.out.println("The home maintenance cost for the fall season is " + fall);
+        System.out.println("The home maintenance cost for the winter season is " + winter);
+        System.out.println("The yearly home maintenance cost is " + total);
+    
+
+
+
+
+
+
+    }
+}
